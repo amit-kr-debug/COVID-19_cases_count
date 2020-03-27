add1537c0de7f1a31dc5a0a741672d967ee3e1ac
Amit Kumar <amitkr0819@gmail.com>
2020-03-27 13:52:45 +0530

    update 5

--- scrappingValues.py
+++ scrappingValues.py
@@ -8,22 +8,45 @@ htmlContent=r.content
 # Parsing the HTML
 
 soup=BeautifulSoup(htmlContent,'html.parser')
-statewises=soup.find_all("td")
+#new code for state data
+# stateName='3'
+# State_name=int(stateName)-1
+# tempData = ""
+# for t in soup.find_all('tbody')[7].find_all('tr'):
+#     tempData += t.get_text()
+# tempData = tempData[1:]
+# StateList = []
+# for i in tempData.split("\n\n"):
+#     StateList.append(i.split("\n"))
+# state_wise_cases=StateList[State_name][2]
+# state_name=StateList[State_name][1]
+
+
+#old code for state data
 # stateName='1'
 # State_name=int(stateName)-1
 # statewises = soup.find_all("td")
 # state_wise_cases=str(statewises[228])
 # state_name=str(statewises[227])
-# print(state_name, state_wise_cases)
+#print(state_name, state_wise_cases)
 def nationalCases():
     nationwide = soup.find_all("div")
     nation_wide_cases = int((nationwide[31].text)[1:6])
     return nation_wide_cases
 def statewise(stateName):
     State_name=int(stateName)-1
-    statewises = soup.find_all("td")
-    state_wise_cases=str(statewises[228+6*State_name].text)
-    state_name=str(statewises[227+6*State_name].text)
+    tempData = ""
+    for t in soup.find_all('tbody')[7].find_all('tr'):
+        tempData += t.get_text()
+    tempData = tempData[1:]
+    StateList = []
+    for i in tempData.split("\n\n"):
+        StateList.append(i.split("\n"))
+    state_wise_cases=StateList[State_name][2]
+    state_name=StateList[State_name][1]
+    # statewises = soup.find_all("td")
+    # state_wise_cases=str(statewises[228+6*State_name].text)
+    # state_name=str(statewises[227+6*State_name].text)
 
     return state_wise_cases,state_name
 
