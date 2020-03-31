d5a1b93296218bb3ed91579af24be43f9f039fdb
Amit Kumar <amitkr0819@gmail.com>
2020-04-01 02:32:35 +0530

    state and nation wise scrapping update

--- scrappingValues.py
+++ scrappingValues.py
@@ -9,19 +9,19 @@ htmlContent=r.content
 
 soup=BeautifulSoup(htmlContent,'html.parser')
 #new code for state data
-stateName='3'
-State_name=int(stateName)-1
-tempData = ""
-# for t in soup.find_all('tbody')[9].find_all('tr'):
+# stateName='28'
+# State_name=int(stateName)
+# tempData = ""
+# for t in soup.find_all('section',{'id':'state-data','class':'site-update'})[0].find_all('tr'):
 #     tempData += t.get_text()
-# tempData = tempData[1:]
 # StateList = []
 # for i in tempData.split("\n\n"):
 #     StateList.append(i.split("\n"))
-# state_wise_cases=StateList[State_name]
-# state_name=StateList[State_name]
-
-
+# total_confirmed=StateList[28][1]
+# death_cases=StateList[29][0]
+# recovered_cases=StateList[30][1]
+#
+#
 #old code for state data
 # stateName='1'
 # State_name=int(stateName)-1
@@ -29,21 +29,42 @@ tempData = ""
 # state_wise_cases=str(statewises[228])
 # state_name=str(statewises[227])
 # print(state_name, state_wise_cases,"\n",StateList)
+# print(tempData)
+# print(total_confirmed,death_cases,recovered_cases)
 def nationalCases():
     nationwide = soup.find_all("div")
-    nation_wide_cases = int((nationwide[31].text)[1:6])
-    return nation_wide_cases
-def statewise(stateName):
-    State_name=int(stateName)-1
+    active_cases = int((nationwide[31].text)[1:6])
     tempData = ""
-    for t in soup.find_all('tbody')[9].find_all('tr'):
+    for t in soup.find_all('section', {'id': 'state-data', 'class': 'site-update'})[0].find_all('tr'):
         tempData += t.get_text()
-    tempData = tempData[1:]
     StateList = []
     for i in tempData.split("\n\n"):
         StateList.append(i.split("\n"))
-    state_wise_cases=StateList[State_name][2]
-    state_name=StateList[State_name][1]
+    total_confirmed = StateList[28][1]
+    death_cases = StateList[29][0]
+    recovered_cases = StateList[30][1]
+    return active_cases,total_confirmed,death_cases,recovered_cases
+def statewise(stateName):
+    State_name=int(stateName)-1
+    tempData = ""
+    for t in soup.find_all('section', {'id': 'state-data', 'class': 'site-update'})[0].find_all('tr'):
+        tempData += t.get_text()
+    StateList = []
+    for i in tempData.split("\n\n"):
+        StateList.append(i.split("\n"))
+    state_wise_cases = StateList[State_name][2]
+    state_name = StateList[State_name][1]
+    # second code
+    # for t in soup.find_all('tbody')[9].find_all('tr'):
+    #     tempData += t.get_text()
+    # tempData = tempData[1:]
+    # StateList = []
+    # for i in tempData.split("\n\n"):
+    #     StateList.append(i.split("\n"))
+    # state_wise_cases=StateList[State_name][2]
+    # state_name=StateList[State_name][1]
+
+    # first code
     # statewises = soup.find_all("td")
     # state_wise_cases=str(statewises[228+6*State_name].text)
     # state_name=str(statewises[227+6*State_name].text)
--- website.py
+++ website.py
@@ -14,14 +14,14 @@ def sms_reply():
         resp.message("Hey there, I'm an automated bot, I can help you to get updated with realtime no. of active cases \n Select the option\n a. Nation wide\n b. State wise data")
         msg=request.form.get('Body')
     elif msg=='a':
-        nat=str(scrappingValues.nationalCases())
-        resp.message("The total no. of active cases nation wide are "+nat)
+        active,total,death,recovered=str(scrappingValues.nationalCases())
+        resp.message("The no. cases of COVID-19 Nation wide are\nTotal confirmed cases:"+total+"\nWhich includes:-\nActive cases: "+active+"\Death cases: "+death+"\nRecovered: "+recovered)
     elif msg=='b':
-        resp.message("To get the info of State wise corona affected people\nSelect accordingly\n1. Andaman and Nicobar\n2. Andhra Pradesh\n3. Bihar\n4. Chandigarh\n5. Chhattisgarh\n6. Delhi\n7. Goa\n8. Gujarat\n9. Haryana\n10. Himachal Pradesh\n11. Jammu and Kashmir\n12. Karnataka\n13. Kerala\n14. Ladakh\n15. Madhya Pradesh\n16. Maharashtra\n17. Manipur\n18. Mizoram\n19. Odisha\n20. Puducherry\n21. Punjab\n22. Rajasthan\n23. Tamil Nadu\n24. Telengana\n25. Uttarakhand\n26. Uttar Pradesh\n27. West Bengal")
+        resp.message("To get the info of State wise corona affected people\nSelect accordingly\n1. Andhra Pradesh\n2. Andaman and Nicobar\n3. Bihar\n4. Chandigarh\n5. Chhattisgarh\n6. Delhi\n7. Goa\n8. Gujarat\n9. Haryana\n10. Himachal Pradesh\n11. Jammu and Kashmir\n12. Karnataka\n13. Kerala\n14. Ladakh\n15. Madhya Pradesh\n16. Maharashtra\n17. Manipur\n18. Mizoram\n19. Odisha\n20. Puducherry\n21. Punjab\n22. Rajasthan\n23. Tamil Nadu\n24. Telengana\n25. Uttarakhand\n26. Uttar Pradesh\n27. West Bengal")
     elif msg.isdigit():
         if(int(msg)>0 and int(msg)<28):
             statecases,statename=scrappingValues.statewise(msg)
-            resp.message("Total no. of active cases in "+statename+" are "+statecases)
+            resp.message("Total no. of confirmed cases in "+statename+" are "+statecases)
             # resp.message("Ministry Of Health has withdrawn the state-wise data from the official website\nSorry for the inconvenience")
         else :
             resp.message("Invalid request please try giving request \n'Give info'\n and follow the instructions")
