215b65bc64c4a323e72b5a9ff5671af23423a2e2
Amit Kumar <amitkr0819@gmail.com>
2020-04-05 15:10:45 +0530

    migrated cases

--- scrappingValues.py
+++ scrappingValues.py
@@ -37,7 +37,7 @@ def nationalCases():
     death_cases = soup.find_all('li', {'class': 'bg-red'})[0].find_all('strong')[0].get_text()
     recovered_cases = soup.find_all('li', {'class': 'bg-green'})[0].find_all('strong')[0].get_text()
     migrated_cases = soup.find_all('li', {'class': 'bg-orange'})[0].find_all('strong')[0].get_text()
-    total_confirmed = str(int(active_cases)+ int(death_cases)+ int(recovered_cases)+int(migrated_cases))
+    total_confirmed = str(int(active_cases) + int(death_cases) + int(recovered_cases) + int(migrated_cases))
     return active_cases,total_confirmed,death_cases,recovered_cases
 def statewise(stateName):
     State_name=int(stateName)
